d72590981ec56c810c6d1aaed817df3c610babc9
saldestechnology <johan@saldestechnology.com>
2024-12-01 21:33:30 +0100

    Updated function head

--- app/api/search/[query]/route.ts
+++ app/api/search/[query]/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from "@/lib/prisma";
 
 export async function GET(
-  request: NextRequest,
   { params }: { params: { query: string } }
 ) {
   try {
