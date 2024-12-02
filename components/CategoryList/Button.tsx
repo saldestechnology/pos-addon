b95c6a978f53f048814fc3b748c7bedbac36fae7
saldestechnology <johan@saldestechnology.com>
2024-12-02 13:42:28 +0100

    Rounded corners

--- components/CategoryList/Button.tsx
+++ components/CategoryList/Button.tsx
@@ -24,7 +24,7 @@ export default function CategoryListBtn({ id, name }: ButtonProps) {
     <button
       onClick={handleClick}
       type="button"
-      className="flex h-16 w-16 items-center justify-center bg-slate-500 text-xl xl:h-32 xl:w-32 xl:text-base"
+      className="flex h-14 w-14 items-center justify-center rounded-md bg-slate-500 text-xl xl:h-24 xl:w-24 xl:text-base"
     >
       {nameToIconMap[name]}
     </button>
