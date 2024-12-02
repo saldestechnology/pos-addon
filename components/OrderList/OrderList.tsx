8106f591288b51a7a08dccc90186d90390f3274f
saldestechnology <johan@saldestechnology.com>
2024-12-02 01:50:26 +0100

    Updated styling
    - Added plus and minus to the right and left of the quantity
    - Added total order
    - Added Place order button

--- components/OrderList/OrderList.tsx
+++ components/OrderList/OrderList.tsx
@@ -1,35 +1,63 @@
 "use client";
 
 import { useOrder } from "@/context/OrderContext";
+import { FiMinus, FiPlus } from "react-icons/fi";
 
 export default function OrderList() {
-  const { orderItems, removeFromOrder, updateQuantity, total } = useOrder();
+  const { orderItems, updateQuantity, total } = useOrder();
+  console.log("rendering order list");
   return (
-    <div className="col-start-10 col-end-13 row-start-1 row-end-13 bg-emerald-500">
-      {orderItems.map((item) => (
-        <div key={item.product.id} className="flex justify-between p-2">
-          <div className="flex">
-            <button
-              onClick={() => removeFromOrder(item.product.id)}
-              className="text-white"
-            >
-              -
-            </button>
-            <input
-              type="number"
-              value={item.quantity}
-              onChange={(e) =>
-                updateQuantity(item.product.id, parseInt(e.target.value, 10))
-              }
-              className="w-10 text-black"
-            />
-            <span className="text-white">{item.product.name}</span>
+    <div className="col-start-10 col-end-13 row-start-1 row-end-13 flex h-full flex-col border-l-2 border-slate-600 bg-white">
+      <div className="flex-grow">
+        {orderItems.map((item) => (
+          <div key={item.product.id} className="flex justify-between p-2">
+            <div className="flex flex-row">
+              <button
+                onClick={() =>
+                  updateQuantity(item.product.id, item.quantity - 1)
+                }
+                className="flex w-4 items-center justify-center text-black"
+              >
+                <FiMinus />
+              </button>
+              <span className="px-2 text-black">{item.quantity}</span>
+              <button
+                onClick={() =>
+                  updateQuantity(item.product.id, item.quantity + 1)
+                }
+                className="flex w-4 items-center justify-center text-black"
+              >
+                <FiPlus />
+              </button>
+              <span className="max-w-36 overflow-hidden text-ellipsis text-nowrap pl-4 text-black">
+                {item.product.name}
+              </span>
+            </div>
+            <span className="text-black">
+              {item.product.basePrice * item.quantity} kr
+            </span>
           </div>
-          <span className="text-white">
-            {item.product.basePrice * item.quantity} kr
+        ))}
+      </div>
+
+      <div className="border-t-2 border-slate-600 p-4">
+        <div className="flex justify-between pb-4">
+          <span className="font-semibold text-black">Total:</span>
+          <span className="font-semibold text-black">
+            {orderItems.reduce(
+              (sum, item) => sum + item.product.basePrice * item.quantity,
+              0,
+            )}{" "}
+            kr
           </span>
         </div>
-      ))}
+        <button
+          onClick={() => alert("Order placed!")}
+          className="w-full rounded-md bg-blue-600 py-2 text-white hover:bg-blue-700"
+        >
+          Place Order
+        </button>
+      </div>
     </div>
   );
 }
