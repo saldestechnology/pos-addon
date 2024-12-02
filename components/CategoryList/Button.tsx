35ec40b1455a1f9bda1401f5b38a532d177b8d32
saldestechnology <johan@saldestechnology.com>
2024-12-02 01:59:31 +0100

    Added fancy icons

--- components/CategoryList/Button.tsx
+++ components/CategoryList/Button.tsx
@@ -7,6 +7,12 @@ interface ButtonProps {
   name: string;
 }
 
+const nameToIconMap: Record<string, string> = {
+  "Hot Drinks": "☕️",
+  "Cold Drinks": "🥤",
+  "Pastries & Snacks": "🥐",
+};
+
 export default ({ id, name }: ButtonProps) => {
   const router = useRouter();
 
@@ -18,9 +24,9 @@ export default ({ id, name }: ButtonProps) => {
     <button
       onClick={handleClick}
       type="button"
-      className="flex h-16 w-16 items-center justify-center bg-slate-500 text-xs xl:h-32 xl:w-32 xl:text-base"
+      className="flex h-16 w-16 items-center justify-center bg-slate-500 text-xl xl:h-32 xl:w-32 xl:text-base"
     >
-      {name}
+      {nameToIconMap[name]}
     </button>
   );
 };
