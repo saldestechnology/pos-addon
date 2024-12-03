d553be843c15705e0a2f4dd5e77a834b89004b84
saldestechnology <johan@saldestechnology.com>
2024-12-03 02:20:28 +0100

    Updated grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="col-start-2 col-end-10 row-start-1 row-end-12 bg-white"></div>
+    <div className="col-end-20 row-end-25 col-start-4 row-start-1 bg-white px-4 pt-4"></div>
   );
 }
