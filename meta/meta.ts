9a86d13a50668c8ff54355278282abd21947f0d8
saldestechnology <johan@saldestechnology.com>
2024-12-03 16:57:16 +0100

    Updated metadata

---
+++ meta/meta.ts
@@ -0,0 +1,9 @@
+type Meta = {
+  title: string;
+  description: string;
+};
+
+export const meta: Meta = {
+  title: "Point of Sale System",
+  description: "An app build by Johan Öhman Saldes",
+};
